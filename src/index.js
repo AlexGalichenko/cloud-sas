92187277b7449bd8c4e171e9c8b0deb05e8c6f30
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2021-04-15 12:38:59 +0300

    updated API_KEY

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 const { FirebaseDB } = require('./db');
-const firebaseConfig = require('../credentials');
+const firebaseConfig = require('./credentials');
 const db = new FirebaseDB(firebaseConfig);
 
 /**
